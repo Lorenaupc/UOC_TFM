1e7daa3f8894d96324b3b4ead2d358fdfd56ce77
Lorena Arriaga <lorena.arriaga.94@gmail.com>
2019-06-15 23:40:46 +0200

    Added keys

---
+++ Assets/Scripts/CheckKeys.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckKeys : MonoBehaviour {
+
+    public PlayerInventory playerInventory;
+    public InventoryItem key;
+
+    void Update()
+    {
+        if (playerInventory.inventory.Contains(key))
+        {
+            GetComponent<DisableTransitions>().Enable();
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            GetComponent<DisableTransitions>().Disable();
+        }
+        
+    }
+}
