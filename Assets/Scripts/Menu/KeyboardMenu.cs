d6556bb5a799a0f826ba7b39a6caa1ef852dc7b9
Lorena Arriaga <lorena.arriaga.94@gmail.com>
2019-06-30 14:53:28 +0200

    Background Images and tutorial

---
+++ Assets/Scripts/Menu/KeyboardMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeyboardMenu : MonoBehaviour {
+
+
+	void Start () {
+        StartCoroutine(passScene());
+	}
+	
+	private IEnumerator passScene()
+    {
+        yield return new WaitForSeconds(8f);
+        SceneManager.LoadScene("Map");
+    }
+}
